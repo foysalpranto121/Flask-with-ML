21b3ced41d1ff1b3f7b38a7cc80b62fd3a5b4fa1
foysalpranto121 <foysalpranto2002@gmail.com>
2026-04-17 00:41:23 +0600

    your message

--- app2.py
+++ app2.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, render_template
+from flask import Flask, request, render_template, redirect, url_for
 
 app = Flask(__name__)
 
@@ -15,9 +15,17 @@ def submit():
         return render_template('form.html', error="Please fill all fields")
 
     if username == "admin" and password == "12345":
-        return f"Welcome, {username}!"
+        # Redirect to dashboard with username
+        return redirect(url_for('dashboard', user=username))
     else:
         return render_template('form.html', error="Invalid credentials")
+
+@app.route('/dashboard')
+def dashboard():
+    username = request.args.get('user')
+    if not username:
+        return redirect(url_for('login'))
+    return render_template('login_success.html', username=username)
 
 if __name__ == '__main__':
     app.run(debug=True)
---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,11 @@
+from flask import Flask, render_template
+app = Flask(__name__)
+
+@app.route('/')
+def  home():
+    users=['Alice', 'Bob', 'Charlie']
+    return render_template('index.html'
+    , users=users)
+
+if __name__ == '__main__':
+    app.run(debug=True)
